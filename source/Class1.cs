b12d196ea8d63def9b2981b4413dc79c3cf0c147
human-sketch <73455299+human-sketch@users.noreply.github.com>
2022-11-19 01:40:17 +1300

    done fork

---
+++ source/Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+using Eco.FrameworkImpl.Ocl;
+using System.Windows.Threading;
+using System.IO;
+using System.Windows.Input;
+using System.Windows.Media.TextFormatting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace LenovoController
+{
+    class Class1
+    {
+        public float minimumVal;
+        public float maximumVal;
+        int returning;
+        public bool charge;
+        public bool chargeWhenPluggedIn; 
+        
+        public int batteryOnOff()
+        {
+
+
+            bool chargeWhenAvaliable = false;
+            PowerLineStatus status = SystemInformation.PowerStatus.PowerLineStatus; 
+            PowerStatus pwr = SystemInformation.PowerStatus;
+            float batteryLife = ((float)pwr.BatteryLifePercent);
+            Debug.WriteLine(batteryLife);
+            Debug.WriteLine(maximumVal);
+
+            if (batteryLife < minimumVal)
+            {
+                returning = 1;
+                return returning;
+            }
+            else if (batteryLife > maximumVal)
+            {
+                returning = 0;
+                return returning;
+            }
+            else if (batteryLife > minimumVal && maximumVal > batteryLife && batteryLife > minimumVal && charge == true)
+            {
+                returning = 1;
+                return returning;
+            }
+            /*
+            else if (status == PowerLineStatus.Offline && chargeWhenPluggedIn == true)
+            {
+                chargeWhenAvaliable = true;
+                return 0;
+            }
+            else if (chargeWhenAvaliable == true && status == PowerLineStatus.Online && chargeWhenPluggedIn == true)
+            {
+                charge = true;
+                returning = 3;
+                return returning;
+            }
+            else if (chargeWhenAvaliable == false && chargeWhenPluggedIn == true)
+            {
+                charge = false;
+                returning = 3;
+                return returning;
+            }
+            */
+            else
+            {
+                returning = 3;
+                return returning;
+
+            }
+            /*
+            if (maximumVal> batteryLife && batteryLife > minimumVal)
+           {
+
+               returning = false;
+               return returning;
+           }
+           else if (batteryLife < minimumVal)
+           {
+               returning = false;
+               return returning;
+           }
+           else
+           {
+               return true;
+           }
+           */
+
+        }
+       
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- source/MainWindow.xaml.cs
+++ source/MainWindow.xaml.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Windows;
+using System.Management;
 using System.Windows.Controls;
 using LenovoController.Features;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.Windows.Threading;
+using System.Security.Principal;
 
 namespace LenovoController
 {
+
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        Class1 batVal = new Class1();
+        DispatcherTimer dt = new DispatcherTimer();
+        int val = 0; 
+
         private readonly RadioButton[] _alwaysOnUsbButtons;
         private readonly AlwaysOnUsbFeature _alwaysOnUsbFeature = new AlwaysOnUsbFeature();
         private readonly RadioButton[] _batteryButtons;
@@ -31,33 +48,140 @@ namespace LenovoController
             _batteryButtons = new[] {radioConservation, radioNormalCharge, radioRapidCharge};
             _alwaysOnUsbButtons = new[] {radioAlwaysOnUsbOff, radioAlwaysOnUsbOnWhenSleeping, radioAlwaysOnUsbOnAlways};
             Refresh();
+            dt.Interval = TimeSpan.FromSeconds(5);
+            dt.Tick += dtTicker;
+            dt.Start();
+            AdminRelauncher();
         }
 
+        private void AdminRelauncher()
+        {
+            if (!IsRunAsAdmin())
+            {
+                ProcessStartInfo proc = new ProcessStartInfo();
+                proc.UseShellExecute = true;
+                proc.WorkingDirectory = Environment.CurrentDirectory;
+                proc.FileName = Assembly.GetEntryAssembly().CodeBase;
+
+                proc.Verb = "runas";
+
+                try
+                {
+                    Process.Start(proc);
+                    Application.Current.Shutdown();
+                }
+                catch (Exception ex)
+                {
+                    admin.Visibility = Visibility.Visible;
+                    admin.Content = "Error: No Admin!";
+                    customCharge.IsEnabled= false;
+                    MessageBox.Show("This program must be run as an administrator! \n\n" + ex.ToString(), "Error");
+                }
+            }
+        }
+
+        private bool IsRunAsAdmin()
+        {
+            try
+            {
+                admin.Visibility = Visibility.Collapsed;
+                WindowsIdentity id = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(id);
+                customCharge.IsEnabled = true;
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        private void dtTicker(object sender, EventArgs e )
+        {
+
+            if (customCharge.IsChecked == true)
+            {
+                batteryCheck(true);
+            }
+            else if(customCharge.IsChecked == false)
+            {
+                _batteryFeature.SetState((BatteryState)0);
+                batteryCheck(false);
+                
+            }
+            Refresh();
+        }
         private class FeatureCheck
         {
             private readonly Action _check;
             private readonly Action _disable;
+            private readonly Action _diable2; 
 
             internal FeatureCheck(Action check, Action disable)
             {
                 _check = check;
                 _disable = disable;
+                
             }
 
+            
             internal void Check() => _check();
             internal void Disable() => _disable();
         }
 
+        private void batteryCheck(bool isOn)
+        {
+            
+            if (isOn == true)
+            {
+                if (customCharge.IsChecked == true)
+                {
+                    batVal.batteryOnOff();
+                    checkboolval.Content = (batVal.batteryOnOff());
+
+                    if (batVal.batteryOnOff() == 0)
+                    {
+                        _batteryFeature.SetState((BatteryState)0);
+                    }
+                    else if (batVal.batteryOnOff() == 1)
+                    {
+                        _batteryFeature.SetState((BatteryState)1);
+                    }
+                    else if (batVal.batteryOnOff() == 3)
+                    {
+                        Debug.WriteLine("Do nothing");
+                    }
+                    else
+                    {
+                        
+                        _batteryFeature.SetState((BatteryState)0);
+                        Window1 win = new Window1();
+                        win.ErrorText.Content = "Unexcepted Error, Battery Mode Could Not Be Set";
+                        win.Error_Code.Content = "Error Code: 105MAIN ";
+                        win.Show();
+                        this.Close();
+                    }
+                }
+                else if (customCharge.IsChecked == false)
+                {
+                    _batteryFeature.SetState((BatteryState)0);
+                    isOn = false;    
+                }
+            }
+            
+        }
         private void Refresh()
         {
             var features = new[]
             {
+                
                 new FeatureCheck(
                     () => _powerModeButtons[(int) _powerModeFeature.GetState()].IsChecked = true,
                     () => DisableControls(_powerModeButtons)),
                 new FeatureCheck(
                     () => _batteryButtons[(int) _batteryFeature.GetState()].IsChecked = true,
-                    () => DisableControls(_batteryButtons)),
+                    () => DisableControls(_batteryButtons) ),
                 new FeatureCheck(
                     () => _alwaysOnUsbButtons[(int) _alwaysOnUsbFeature.GetState()].IsChecked = true,
                     () => DisableControls(_alwaysOnUsbButtons)),
@@ -84,6 +208,8 @@ namespace LenovoController
                     feature.Disable();
                 }
             }
+            
+
         }
 
         private void DisableControls(Control[] buttons)
@@ -100,11 +226,14 @@ namespace LenovoController
         private void radioPowerMode_Checked(object sender, RoutedEventArgs e)
         {
             _powerModeFeature.SetState((PowerModeState) Array.IndexOf(_powerModeButtons, sender));
+            
         }
 
         private void radioBattery_Checked(object sender, RoutedEventArgs e)
         {
             _batteryFeature.SetState((BatteryState) Array.IndexOf(_batteryButtons, sender));
+            
+            
         }
 
         private void radioAlwaysOnUsb_Checked(object sender, RoutedEventArgs e)
@@ -134,6 +263,80 @@ namespace LenovoController
                 ? FnLockState.On
                 : FnLockState.Off;
             _fnLockFeature.SetState(state);
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            batVal.minimumVal = (float)Minimum_Value.Value;
+           
+            try
+            {
+                Debug.WriteLine(batVal.batteryOnOff());
+                float val3 = batVal.minimumVal * 100;
+                minVal.Content = (int)val3;
+                checkboolval.Content = (batVal.batteryOnOff());
+                
+            }
+            catch
+            {
+                Debug.WriteLine("nothing");
+            }
+
+            
+        }
+
+        private void Maximum_Value_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            batVal.maximumVal = (float)Maximum_Value.Value;
+            try
+            {
+                float val2 = batVal.maximumVal * 100;
+                maxVal.Content = (int)val2 ;
+                checkboolval.Content = (batVal.batteryOnOff());
+                if (Maximum_Value.Value < Minimum_Value.Value + 0.04)
+                {
+                    MessageBox.Show("Maximum charge level cannot be below minimum charge value +5%"+ "\n" + "\n" + "Make sure maximum charge value is 5% higher than minimum charge value", "Error");
+                    Maximum_Value.Value = Minimum_Value.Value + 0.05;
+                }
+            }
+            catch
+            {
+                Debug.WriteLine("error");
+            }
+
+            
+        }
+
+        private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            
+        }
+
+        private void checkBat_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (alwaysCharge.IsChecked == true)
+            {
+
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void helpButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/human-sketch/Lenovo-Controller-Custom-Charge#how-to-use");
         }
     }
 }
